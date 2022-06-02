6b94aff1ac911bc6fa30eeb55c50c6d1a9494a6b
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-06-02 15:07:51 +0530

    Time: 3 ms (63.54%), Space: 10.1 MB (45.30%) - LeetHub

---
+++ 100-same-tree/100-same-tree.cpp
@@ -0,0 +1,19 @@
+
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        if (!p || !q)
+        {
+            return p == q;
+        }
+
+        if (p->val != q->val)
+        {
+            return false;
+        }
+
+        return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
+    }
+};
