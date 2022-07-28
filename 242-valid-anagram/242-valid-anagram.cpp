e73fe3a9886d2cf0f00b013e4da4b4e44da05e4a
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-07-28 09:32:40 +0530

    Time: 21 ms (28.88%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()){
-            return false;
-        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
         
-        vector<int> arr(26, 0);        
-        
-        for(int i = 0; i<s.size(); i++){
-            arr[(int)(s[i]-'a')]++;
-            arr[(int)(t[i]-'a')]--;
-        }
-        
-        for(auto it : arr){
-            if(it != 0){
-                return false;
-            }
-        }
-        
-        return true;
+        return s==t;
     }
 };
