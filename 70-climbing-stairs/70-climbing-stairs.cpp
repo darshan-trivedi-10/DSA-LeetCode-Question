8b47e8a73b1557733c2be24111826b43d01605e9
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-08-22 17:40:58 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (31.68%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -14,11 +14,16 @@ class Solution {
     
 public:
     int climbStairs(int n) {
-        if(n<=0){
-            return n==0;
-        }
         
         vector<int> dp(n+1,-1);
-        return helper(n-1, dp) + helper(n-2, dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i<=n; i++){
+            cout<<i<<" "<<dp[i-1]+dp[i-2]<<endl;
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
     }
 };
