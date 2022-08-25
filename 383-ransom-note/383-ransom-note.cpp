02f4ecd97310ec4b97cd3afcb0e8297b82bf0940
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-08-25 16:51:08 +0530

    Time: 24 ms (56.74%), Space: 8.8 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         int arr[26] = {0};
-        for(int i = 0; i<ransomNote.size(); i++){
-            int pos = ransomNote[i] - 'a';
-            arr[pos]++;
-        }
         
         for(int i = 0; i<magazine.size(); i++){
             int pos = magazine[i] - 'a';
-            if(arr[pos]>0){
-                arr[pos]--;
-            }
+            arr[pos]++;
+            
         }
         
-        for(int i = 0; i<26; i++){
-            if(arr[i] != 0){
+        for(int i = 0; i<ransomNote.size(); i++){
+            int pos = ransomNote[i] - 'a';
+            if(!arr[pos]--){
                 return false;
             }
         }
