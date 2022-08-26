666db26b89ec8dbb28071dd476e29922e8fbcb7a
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-08-26 18:56:20 +0530

    Time: 9 ms (7.55%), Space: 7.6 MB (77.09%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,34 +1,26 @@
+// Tabulation + Space optimization
 class Solution {
-    
-    int robMax(vector<int> &nums, vector<int> &dp, int idx, int n){
-        if(idx>=n){ 
-            return 0;
-        }
-        
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        
-        int firstHouseRob = robMax(nums,dp,idx+2,n) + nums[idx];
-        int secondHouseRob = robMax(nums,dp,idx+1,n);
-        
-        return dp[idx] = max(firstHouseRob,secondHouseRob);
-    }
     
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int> dp(n, -1);
-        dp[0] = nums[0];
+        // vector<int> dp(n, -1);
+        // dp[0] = nums[0];
+        int prev = nums[0], prev2;
         for(int i = 1; i<n; i++){
-            dp[i] = max(dp[i-1], nums[i]);
+            // dp[i] = max(dp[i-1], nums[i]);
+            int curr = max(prev, nums[i]);
             if(i>1){ 
-                dp[i] = max(dp[i-2] + nums[i], dp[i]);
+                // dp[i] = max(dp[i-2] + nums[i], dp[i]);
+                curr = max(prev2 + nums[i], curr);
             }
+            
+            prev2 = prev;
+            prev = curr;
         }
         
-        return dp[n-1];
+        return prev;
     }
 };
 
@@ -63,4 +55,25 @@ public:
         return robMax(nums, dp, 0, n);
     }
 };
+
+// Using Tabulation
+class Solution {
+    
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> dp(n, -1);
+        dp[0] = nums[0];
+        for(int i = 1; i<n; i++){
+            dp[i] = max(dp[i-1], nums[i]);
+            if(i>1){ 
+                dp[i] = max(dp[i-2] + nums[i], dp[i]);
+            }
+        }
+        
+        return dp[n-1];
+    }
+};
+
 */
