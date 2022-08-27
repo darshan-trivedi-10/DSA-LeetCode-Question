c6739a99afda8e5b31cff8fff6e65135593f6f62
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-08-27 18:29:37 +0530

    Time: 7 ms (6.65%), Space: 6.6 MB (26.81%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,32 +1,27 @@
 class Solution {
     
-    bool isValid(int n, int m, int i, int j){
-        return i>=0 and j>=0 and i<n and j<m;
-    }
-    
-    int findPath(vector<vector<int>> &dp, int n, int m, int i, int j){
-        if(i==n-1 || j==m-1){
+    int findPath(int i, int j, vector<vector<int>> &dp){
+        if(i==0 and j==0){
             return 1;
         }
         
-        if(isValid(n,m,i,j)==false){
-            return false;
+        if(i<0 || j<0){
+            return 0;
         }
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         
-        int downPath = findPath(dp,n,m,i+1,j);
-        int rightPath = findPath(dp,n,m,i,j+1);
+        int up = findPath(i-1,j, dp);
+        int left = findPath(i, j-1, dp);
         
-        return dp[i][j] = downPath + rightPath;
+        return dp[i][j] = up + left;
     }
-    
     
 public:
     int uniquePaths(int n, int m) {
         vector<vector<int>> dp(n, vector<int> (m,-1));
-        return findPath(dp,n,m,0,0);
+        return findPath(n-1, m-1,dp);
     }
 };
