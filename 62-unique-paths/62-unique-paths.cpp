0233491356921abd90c4ff12fdd9598825cf410c
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-08-27 18:36:53 +0530

    Time: 0 ms (100.00%), Space: 6 MB (75.36%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -21,7 +21,26 @@ class Solution {
     
 public:
     int uniquePaths(int n, int m) {
-        vector<vector<int>> dp(n, vector<int> (m,-1));
-        return findPath(n-1, m-1,dp);
+        int dp[n][m];
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(i==0 and j==0){
+                    dp[i][j] = 1;
+                }else{
+                    int up = 0, left = 0;
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0){
+                        left = dp[i][j-1];
+                    }
+                    dp[i][j] = up+left;
+                }              
+            }
+        }
+        
+        
+        return dp[n-1][m-1];
     }
 };
