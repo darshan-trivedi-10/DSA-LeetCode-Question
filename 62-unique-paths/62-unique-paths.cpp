320b46cb3a988b5faef8f7eb36e1ba1eff5bca88
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-08-27 18:21:50 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.81%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,32 +1,32 @@
-
-class Solution
-{
-    int findPath(vector<vector<int>> &dp, int m, int n, int i, int j)
-    {
-        if (i == n - 1 && j == m - 1)
-        {
+class Solution {
+    
+    bool isValid(int n, int m, int i, int j){
+        return i>=0 and j>=0 and i<n and j<m;
+    }
+    
+    int findPath(vector<vector<int>> &dp, int n, int m, int i, int j){
+        if(i==n-1 || j==m-1){
             return 1;
         }
-        if (i >= n || j >= m || i < 0 || j < 0)
-        {
-            return 0;
+        
+        if(isValid(n,m,i,j)==false){
+            return false;
         }
-
-        if (dp[i][j] != -1)
-        {
+        
+        if(dp[i][j] != -1){
             return dp[i][j];
         }
-
-        int right = findPath(dp, m, n, i, j + 1);
-        int bottom = findPath(dp, m, n, i + 1, j);
-        return dp[i][j] = right + bottom;
+        
+        int downPath = findPath(dp,n,m,i+1,j);
+        int rightPath = findPath(dp,n,m,i,j+1);
+        
+        return dp[i][j] = downPath + rightPath;
     }
-
+    
+    
 public:
-    int uniquePaths(int m, int n)
-    {
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        int totalPath = findPath(dp, m, n, 0, 0);
-        return totalPath;
+    int uniquePaths(int n, int m) {
+        vector<vector<int>> dp(n, vector<int> (m,-1));
+        return findPath(dp,n,m,0,0);
     }
 };
