fcc15429d32d60353f0363da2952e8cbd5f6db35
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-08-29 21:41:31 +0530

    Time: 11 ms (48.46%), Space: 9.1 MB (10.13%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,27 +1,31 @@
 class Solution {
-    int findMinPath(vector<vector<int>> &triangle, vector<vector<int>> &dp, int i, int j) {
+    
+    int findMinSumPath(vector<vector<int>> &triangle, vector<vector<int>> &dp, int i, int j){
         int n = triangle.size(), m = triangle[0].size();
-        if (i == n - 1)
-        {
+        
+        if(j>=n){
+            return INT_MAX;
+        } 
+        
+        if(i==n-1){
             return triangle[i][j];
         }
-
-        if (dp[i][j] != INT_MIN)
-        {
+        
+        if(dp[i][j] != -1){
             return dp[i][j];
         }
-
-        int bottom = findMinPath(triangle, dp, i + 1, j);
-        int bottomNext = findMinPath(triangle, dp, i + 1, j + 1);
-
-        return dp[i][j] = triangle[i][j] + min(bottom, bottomNext);
+        
+        int bottom = findMinSumPath(triangle, dp, i+1, j);
+        int bottomSide = findMinSumPath(triangle, dp, i+1, j+1);
+        
+        return dp[i][j] = triangle[i][j] + min(bottom, bottomSide);
     }
     
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        int m = triangle[n-1].size();
-        vector<vector<int>> dp(n,vector<int> (n, INT_MIN));
-        return findMinPath(triangle, dp, 0, 0);
+        int n = triangle.size(), m = triangle[n-1].size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        
+        return findMinSumPath(triangle, dp, 0, 0);
     }
 };
