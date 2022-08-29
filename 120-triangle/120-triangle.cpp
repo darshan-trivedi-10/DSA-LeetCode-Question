da347f847df0803368ec4f1b4293248eaf5e89a9
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-08-29 21:58:00 +0530

    Time: 14 ms (26.14%), Space: 8.9 MB (51.86%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -15,10 +15,10 @@ class Solution {
             return dp[i][j];
         }
         
-        int bottom = findMinSumPath(triangle, dp, i+1, j);
-        int bottomSide = findMinSumPath(triangle, dp, i+1, j+1);
+        int down = findMinSumPath(triangle, dp, i+1, j);
+        int diagonal = findMinSumPath(triangle, dp, i+1, j+1);
         
-        return dp[i][j] = triangle[i][j] + min(bottom, bottomSide);
+        return dp[i][j] = triangle[i][j] + min(down, diagonal);
     }
     
 public:
@@ -26,6 +26,20 @@ public:
         int n = triangle.size(), m = triangle[n-1].size();
         vector<vector<int>> dp(n, vector<int>(m, -1));
         
-        return findMinSumPath(triangle, dp, 0, 0);
+        for(int i = n-1; i>=0; i--){
+            for(int j = triangle[i].size()-1; j>=0; j--){
+                if(i==n-1){
+                    dp[i][j] = triangle[i][j];
+                }else{
+                    int down = dp[i+1][j], diagonal = dp[i+1][j+1];
+                    dp[i][j] = triangle[i][j] + min(down, diagonal);
+                }
+            }
+        }
+        
+        return dp[0][0];
+        
+        
+        // return findMinSumPath(triangle, dp, 0, 0);
     }
 };
