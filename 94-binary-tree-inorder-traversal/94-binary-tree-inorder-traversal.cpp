6db80b603fd9dffd5c672142951afff09f8bfe50
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-09-08 17:32:20 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (75.10%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,40 +1,30 @@
-
-class Solution
-{
-public:
-    vector<int> inorderTraversal(TreeNode *root)
-    {
-        vector<int> inorder;
-        TreeNode *curr = root;
-
-        while (curr != NULL)
-        {
-            if (curr->left == NULL)
-            {
-                inorder.push_back(curr->val);
-                curr = curr->right;
-            }
-            else
-            {
-                TreeNode *prev = curr->left;
-                while (prev->right and prev->right != curr)
-                {
-                    prev = prev->right;
-                }
-                if (prev->right == NULL)
-                {
-                    prev->right = curr;
-                    curr = curr->left;
-                }
-                else
-                {
-                    prev->right = NULL;
-                    inorder.push_back(curr->val);
-                    curr = curr->right;
-                }
-            }
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+    void inorder(TreeNode* root,vector<int> &ans){
+        if(root==NULL){
+            return;
         }
-
-        return inorder;
+        
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);        
+    }
+    
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorder(root, ans);
+        return ans;
     }
 };
