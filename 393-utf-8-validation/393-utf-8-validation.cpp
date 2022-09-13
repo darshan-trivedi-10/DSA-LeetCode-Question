e5ba7214d69ad137551b61afaa2438d862f02822
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-09-13 20:28:21 +0530

    Time: 24 ms (37.41%), Space: 14 MB (76.31%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,24 @@
+// Space Complexity: O(1) 
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int remaining = 0;
+        for (auto& x : data) {
+            if (remaining == 0) {
+                if ((x >> 5) == 0b110) {
+                    remaining = 1;
+                } else if ((x >> 4) == 0b1110) {
+                     remaining = 2;
+                } else if ((x >> 3) == 0b11110) {
+                    remaining = 3;
+                } else if ((x >> 7) != 0) {
+                    return false;
+                }
+            } else {
+                if ((x >> 6) != 0b10) return false;
+                else remaining--;
+            }
+        }
+        return remaining == 0;
+    }
+};
