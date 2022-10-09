263553fb5fa1f5ed3f92eac2f1aece4b0f6864bd
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-10-09 08:57:52 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.84%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        
-        return __builtin_popcount(n);
+        // return __builtin_popcount(n);
         int ans = 0;
         while(n>0){
             if(n&1){
