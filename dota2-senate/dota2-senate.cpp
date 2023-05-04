2ea5775c07d3021d49f7d823f02ddd6969c3907b
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-05-04 10:15:18 +0530

    Update dota2-senate.cpp
    added one more solutions

--- dota2-senate/dota2-senate.cpp
+++ dota2-senate/dota2-senate.cpp
@@ -1,3 +1,37 @@
+// Solution 1
+
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        const int n = senate.size();
+        queue<int> dqueue, rqueue;
+
+        for(int i = 0; i < n; i++){
+            char ch = senate[i];
+            if(ch=='R'){
+                rqueue.push(i);
+            }else{
+                dqueue.push(i);
+            }
+        }
+
+        while(!dqueue.empty() && !rqueue.empty()){
+            int rTurn = rqueue.front(), dTurn = dqueue.front();
+            rqueue.pop(), dqueue.pop();
+
+            if(rTurn < dTurn){
+                rqueue.push(rTurn + n);
+            }else{
+                dqueue.push(dTurn + n);
+            }
+
+        }
+
+        return rqueue.empty() ? "Dire" : "Radiant";
+    }
+};
+
+// Solution 2
 class Solution {
 public:
     string predictPartyVictory(string senate) {
