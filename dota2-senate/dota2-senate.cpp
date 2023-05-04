e541ab5d24b2387fa53ee24565e65ccd48c596d8
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-05-04 10:14:25 +0530

    Time: 12 ms (33.59%) | Memory: 7.3 MB (71.76%) - LeetSync

--- dota2-senate/dota2-senate.cpp
+++ dota2-senate/dota2-senate.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
     string predictPartyVictory(string senate) {
+
+        const string DIRE = "Dire", RADIANT = "Radiant";
         const int n = senate.size();
-        queue<int> dqueue, rqueue;
-
+        
+        int rCount = 0, dCount = 0, rBan = 0, dBan = 0;
+        queue<char> q;
         for(int i = 0; i < n; i++){
-            char ch = senate[i];
-            if(ch=='R'){
-                rqueue.push(i);
-            }else{
-                dqueue.push(i);
-            }
+            q.push(senate[i]);
+            senate[i]=='R' ? rCount++ : dCount++;
         }
 
-        while(!dqueue.empty() && !rqueue.empty()){
-            int rTurn = rqueue.front(), dTurn = dqueue.front();
-            rqueue.pop(), dqueue.pop();
+        while(rCount>0 && dCount>0){
+            char ch = q.front();
+            q.pop();
 
-            if(rTurn < dTurn){
-                rqueue.push(rTurn + n);
+            if(ch == 'R'){
+                if(rBan){
+                    rBan--;
+                    rCount--;
+                }else{
+                    dBan++;
+                    q.push('R');
+                }
             }else{
-                dqueue.push(dTurn + n);
+                if(dBan){
+                    dBan--;
+                    dCount--;
+                }else{
+                    rBan++;
+                    q.push('D');
+                }
             }
 
         }
 
-        return rqueue.empty() ? "Dire" : "Radiant";
+        return rCount > 0 ? RADIANT : DIRE;
     }
 };
