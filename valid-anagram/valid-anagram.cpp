7f98c88086c63a15e350de8ca6310d6e05a41e48
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-05-06 16:37:31 +0530

    Time: 17 ms (20.07%) | Memory: 7.4 MB (15.49%) - LeetSync

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-
-        return s == t;
+        int ns = s.length(), nt = t.length();
+        if(ns != nt) return false;
+        unordered_map<char,int> mp;
+        for(int i = 0;i<ns;i++) mp[s[i]]++;
+        
+        for(int i = 0;i<nt;i++){
+            if(mp.find(t[i]) == mp.end() || mp[t[i]] <= 0) return false;
+            else mp[t[i]]--;
+        }
+        return true;
     }
 };
