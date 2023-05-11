681efaa28f3a2ce382fd5a71c310213e0c11da3c
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-05-11 17:37:26 +0530

    Time: 150 ms (47.75%) | Memory: 50 MB (24.56%) - LeetSync

--- number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,22 +1,42 @@
+
+
 class Solution {
+
+    static int multiply(int n){
+        int start = 1, mod = 1000000007;
+        for(int i = 1; i <= n; i++){
+            start = (start * 2)%mod;
+        }
+
+        return (start%mod);
+    }
+
+
 public:
     int numSubseq(vector<int>& nums, int target) {
-        int res = 0, mod = 1000000007, l = 0, r = nums.size() - 1;
-        vector<int> pre = {1};
-        for (auto i = 1; i <= nums.size(); ++i){
-            pre.push_back((pre.back() << 1) % mod);
-        }   
-                
-        sort(begin(nums), end(nums));
-        
-        while (l <= r) {
-            if (nums[l] + nums[r] > target) {
+        const int n = nums.size(), mod = 1000000007;
+
+        vector<int> mul(n + 1, 1);
+        for(int i = 1; i <=n; i++){
+            mul[i] = (2 * mul[i - 1])%mod;
+        }
+
+
+        sort(nums.begin(), nums.end());
+        int l = 0, r = n-1, ans = 0;
+        while(l <= r){
+            if(nums[l] + nums[r] > target){
                 r--;
-            } else {
-                res = (res + pre[r - l++]) % mod;
+            }else{
+                if(r == l){
+                    ans++;
+                }else{
+                    ans = (ans + mul[r - l]) % mod;
+                }
+                l++;
             }
         }
 
-        return res;
+        return ans;
     }
 };
