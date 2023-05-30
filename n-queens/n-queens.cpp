27abb69a61d4f2d0e80b99a2ea42da3ff91f8123
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-05-30 11:30:46 +0530

    Time: 8 ms (59.84%) | Memory: 7.1 MB (78.25%) - LeetSync

--- n-queens/n-queens.cpp
+++ n-queens/n-queens.cpp
@@ -1,73 +1,31 @@
 class Solution {
-
-    bool is_safe(vector<string> &board, int row, int col){
-        int n = board.size();
-        for(int i = 0; i < n; i++){
-            if(board[i][col] == 'Q' && i != row){
-                return false;
-            }
-            if(board[row][i] == 'Q' && i != col){
-                return false;
-            }
-        }
-
-        int count = 0, i = row + 1, j = col - 1;
-        while(i < n && j >= 0){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-            i++, j--;
-        }
-
-        i = row + 1, j = col + 1;
-        while(i < n && j < n){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-            i++, j++;
-        }
-        
-        i = row - 1, j = col - 1;
-
-        while(i >= 0 && j >= 0){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-            i--, j--;
-        }
-
-        i = row - 1, j = col + 1;
-        while(i >= 0 && j < n){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-            i--, j++;
-        }
-
-        return true;
-    }
-
-    void generate(int n, int row, vector<vector<string>> &boards, vector<string> &board){
-        if(row == n){
-            boards.push_back(board);
+    
+    void Solver(int n, vector<vector<string>> &ans, vector<string> &board, int col, vector<bool> &leftrow, vector<bool> &upperDiagnoal, vector<bool> &lowerDiagnoal){
+        if(col==n){
+            ans.push_back(board);
             return;
         }
-       
-        for(int col = 0; col < n; col++){
-            board[row][col] = 'Q';
-            if(is_safe(board, row, col)){
-                generate(n, row + 1, boards, board);
+        
+        for(int row = 0; row<n; row++){
+            if(!leftrow[row] and !lowerDiagnoal[row+col] and !upperDiagnoal[n-1+col-row]){
+                leftrow[row] = lowerDiagnoal[row+col] = upperDiagnoal[n-1+col-row] = true;
+                board[row][col] = 'Q';
+                Solver(n, ans, board, col+1, leftrow, upperDiagnoal, lowerDiagnoal);
+                board[row][col] = '.';
+                leftrow[row] = lowerDiagnoal[row+col] = upperDiagnoal[n-1+col-row] = false;   
             }
-            board[row][col] = '.';
         }
-    }    
 
+    }
+    
+    
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> boards;
-        string s(n, '.');
-        vector<string> board(n, s);
-        generate(n, 0, boards, board);
-        return boards;
+        vector<vector<string>> ans;
+        string s(n,'.');
+        vector<string> board(n,s);
+        vector<bool> leftrow(n, false), upperDiagnoal(2*n-1, false), lowerDiagnoal(2*n-1, false); 
+        Solver(n, ans, board, 0, leftrow, upperDiagnoal,lowerDiagnoal);
+        return ans;
     }
 };
