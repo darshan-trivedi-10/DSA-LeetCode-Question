29d787b02f482b2455529f9e083cb12dac323295
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-05-31 09:06:42 +0530

    Time: 152 ms (77.03%) | Memory: 59.2 MB (44.32%) - LeetSync

--- design-underground-system/design-underground-system.cpp
+++ design-underground-system/design-underground-system.cpp
@@ -1,27 +1,41 @@
+#include <unordered_map>
+#include <string>
+
 class UndergroundSystem {
+    struct CustomerTrack {
+        std::string stationName;
+        int checkInTime;
+    };
+
+    std::unordered_map<int, CustomerTrack> customerTrack;
+    std::unordered_map<std::string, std::pair<double, int>> averageTime;
+
 public:
-    UndergroundSystem() {
-        
-    }
-    
-    void checkIn(int id, std::string stationName, int t) {
-        checkInData[id] = {stationName, t};
-    }
-    
-    void checkOut(int id, std::string stationName, int t) {
-        auto& [startStation, startTime] = checkInData[id];
-        std::string route = startStation + "_" + stationName;
-        totalTime[route] += t - startTime;
-        tripCount[route]++;
-    }
-    
-    double getAverageTime(std::string startStation, std::string endStation) {
-        std::string route = startStation + "_" + endStation;
-        return static_cast<double>(totalTime[route]) / tripCount[route];
+    UndergroundSystem() {}
+
+    void checkIn(int id, const std::string& stationName, int t) {
+        customerTrack[id] = {stationName, t};
     }
 
-private:
-    std::unordered_map<int, std::pair<std::string, int>> checkInData;
-    std::unordered_map<std::string, int> totalTime;
-    std::unordered_map<std::string, int> tripCount;
+    void checkOut(int id, const std::string& stationName, int t) {
+        const auto& track = customerTrack[id];
+        double totalTime = t - track.checkInTime;
+        std::string key = track.stationName + "_" + stationName;
+        averageTime[key].first += totalTime;
+        averageTime[key].second++;
+    }
+
+    double getAverageTime(const std::string& startStation, const std::string& endStation) {
+        std::string key = startStation + "_" + endStation;
+        const auto& timeData = averageTime[key];
+        return timeData.first / timeData.second;
+    }
 };
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
