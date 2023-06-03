c906e3825b55946754836e8eb3e1ea2a98c95aaf
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-06-03 08:53:14 +0530

    Time: 35 ms (75.47%) | Memory: 71.4 MB (38.18%) - LeetSync

--- add-two-numbers/add-two-numbers.cpp
+++ add-two-numbers/add-two-numbers.cpp
@@ -1,44 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *ans = new ListNode(-1);
-        ListNode *temp = ans;
-        int carry = 0;
-        
-        while(l1 != NULL && l2 != NULL){
-            int currSum = l1->val + l2->val + carry;
-            ans -> next = new ListNode(currSum%10);
-            carry = currSum/10;
-            l1 = l1->next;
-            l2 = l2->next;
-            ans = ans ->next;
-        }
-        
-        while(l1 != NULL){
-            int currSum = l1->val + carry;
-            ans -> next = new ListNode(currSum%10);
-            carry = currSum/10;
-            l1 = l1->next;
-            ans = ans ->next;
-        }
-        
-        while(l2 != NULL){
-            int currSum = l2->val + carry;
-            ans -> next = new ListNode(currSum%10);
-            carry = currSum/10;
-            l2 = l2->next; 
-            ans = ans ->next;
+        ListNode *head = new ListNode(-1);
+        ListNode *tail = head;
 
+        int carry = 0;
+        while(l1 != NULL && l2 != NULL){
+            int sum = l1->val + l2->val + carry;
+            tail->next = new ListNode(sum%10);
+            tail = tail->next;
+            l1 = l1->next, l2 = l2->next;
+            carry = sum/10;
         }
-        
-        while(carry>0){
-            ans -> next = new ListNode(carry%10);
+
+        while(l1 != NULL){
+            int sum = l1->val + carry;
+            tail->next = new ListNode(sum%10);
+            tail = tail->next;
+            l1 = l1->next;
+            carry = sum/10;
+        }
+
+        while(l2 != NULL){
+            int sum = l2->val + carry;
+            tail->next = new ListNode(sum%10);
+            tail = tail->next;
+            l2 = l2->next;
+            carry = sum/10;
+        }
+
+        while(carry > 0){
+            tail->next = new ListNode(carry%10);
+            tail = tail->next;
             carry = carry/10;
-            ans = ans ->next;
         }
-        
-        
-        return temp->next;
-   
+
+        return head->next;
     }
 };
