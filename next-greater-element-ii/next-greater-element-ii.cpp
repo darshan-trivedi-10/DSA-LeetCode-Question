82c2f460a32d585af0b5b8413bc14c629fcfa6a5
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-06-08 19:05:14 +0530

    Time: 39 ms (65.84%) | Memory: 23.9 MB (54.70%) - LeetSync

--- next-greater-element-ii/next-greater-element-ii.cpp
+++ next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& nums) {
+    std::vector<int> nextGreaterElements(const std::vector<int>& nums) {
         const int size = nums.size();
-        stack<int> st;
-        vector<int> greaterElement(size, -1); 
-        for(int i =  2 * size - 1; i >=0; i--){
-            int idx = i%size;
-            if(!st.empty()){
-                if(st.top() > nums[idx]){
-                    greaterElement[idx] = st.top();
-                }else{
-                    while(!st.empty()){
-                        if(st.top() > nums[idx]){
-                            greaterElement[idx] = st.top();
-                            break;
-                        }
-                        st.pop();
-                    }
-                }
+        stack<int> numStack;
+        vector<int> greaterElements(size, -1);
+
+        for (int i = 2 * size - 1; i >= 0; i--) {
+            int index = i % size;
+            while (!numStack.empty() && numStack.top() <= nums[index]) {
+                numStack.pop();
             }
-            st.push(nums[idx]);
+            if (!numStack.empty()) {
+                greaterElements[index] = numStack.top();
+            }
+            numStack.push(nums[index]);
         }
 
-        return greaterElement;
+        return greaterElements;
     }
 };
