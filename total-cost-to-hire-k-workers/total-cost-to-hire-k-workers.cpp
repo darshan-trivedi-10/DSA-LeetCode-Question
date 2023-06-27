789deff96f944b3a674fa4ea76e7c08c8cc588b1
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-06-27 07:51:07 +0530

    Time: 195 ms (82.82%) | Memory: 72.8 MB (82.07%) - LeetSync

--- total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
+++ total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
@@ -1,53 +1,33 @@
-class Solution {
+class Solution
+{
 public:
-    long long totalCost(vector<int>& costs, int k, int candidates) {
+    long long totalCost(vector<int> &costs, int k, int candidates)
+    {
         const int size = costs.size();
+        int i = 0, j = size - 1;
+        
         long long ans = 0;
-
-        if((candidates * 2) >= size){
-            sort(costs.begin(), costs.end());
-            for(int i = 0; i < k; i++){
-                ans += costs[i];
-            }
-        }else{
-            priority_queue<int, vector<int>, greater<int>> left, right;
-            int i = 0, j = size - 1;
-            while(k > 0 && i <= j){
-                if(left.size() < candidates){
-                    left.push(costs[i]);
-                    i++;
-                }
-
-                if(right.size() < candidates){
-                    right.push(costs[j]);
-                    j--;
-                } 
-
-                if(left.size() == candidates && right.size() == candidates){
-                    if(left.top() <= right.top()){
-                        ans += left.top();
-                        cout<<left.top()<<" ";
-                        left.pop();
-                        k--;
-                    }else{
-                        ans += right.top();
-                        cout<<right.top()<<" ";
-                        right.pop();
-                        k--;
-                    }
-                }
+        priority_queue<int, vector<int>, greater<int>> left, right;
+        while(k--){
+            while(left.size() < candidates && i <= j){
+                left.push(costs[i]);
+                i++;
             }
 
-            if(k > 0){
-                while(!right.empty()){
-                    left.push(right.top());
-                    right.pop();
-                }
-                while(k > 0){
-                    ans += left.top();
-                    left.pop();
-                    k--;
-                }
+            while(right.size() < candidates && i <= j){
+                right.push(costs[j]);
+                j--;
+            }
+
+            int e1 = left.size() > 0 ? left.top() : INT_MAX;
+            int e2 = right.size() > 0 ? right.top() : INT_MAX;
+
+            if(e1 <= e2){
+                ans += e1;
+                left.pop();
+            }else{
+                ans += e2;
+                right.pop();
             }
         }
 
