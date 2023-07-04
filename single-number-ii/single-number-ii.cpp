5009bfd29db61e508ad59a3fcd121134c5689fb9
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-07-04 14:33:39 +0530

    Time: 19 ms (6.10%) | Memory: 9.4 MB (98.52%) - LeetSync

--- single-number-ii/single-number-ii.cpp
+++ single-number-ii/single-number-ii.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        const int size = nums.size();
+        int size = nums.size();
 
-        int ans = 0;
-        for(int i = 0; i < 32; i++){
-            int sum = 0;
-            for(int j = 0; j < size; j++){
-                if(((nums[j] >> i) & 1) == 1){
-                    sum++;
+        vector<int> bits(32, 0);
+        for(int i = 0; i < size; i++){
+            int ele = nums[i];
+            for(int j = 0; j < 32; j++){
+                if((1 << j) & nums[i]){
+                    bits[j]++;
                 }
-            }
-
-            if(sum%3){
-                ans = ans | 1 << i;
             }
         }
 
-        return ans;
+        int singleNum = 0;
+        for(int i = 0; i < 32; i++){
+            if(bits[i]%3 != 0){
+                singleNum = singleNum | (1 << i);
+            }
+        }
 
-
+        return singleNum;
     }
 };
+/*
+5 = 101
+(1 >> j)
+
+*/
