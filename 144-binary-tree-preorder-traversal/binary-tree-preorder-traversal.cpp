df17cb3cf1f860d4f8301f281c2f0af768430c9d
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-07-18 14:16:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (27.21%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -10,20 +10,28 @@
  * };
  */
 class Solution {
-    void traversalHelper(TreeNode *root, vector<int> &nodes){
-        if(root == NULL){
-            return;
-        }
-
-        nodes.push_back(root->val);
-        traversalHelper(root->left, nodes);
-        traversalHelper(root->right, nodes);
-    }
-
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> nodes;
-        traversalHelper(root, nodes);
+        if(root == NULL){
+            return nodes;
+        }
+
+        stack<TreeNode *> st;
+        st.push(root);
+
+        while(!st.empty()){
+            TreeNode *temp = st.top();
+            st.pop();
+            nodes.push_back(temp->val);
+            if(temp->right != NULL){
+                st.push(temp->right);
+            }
+            if(temp->left != NULL){
+                st.push(temp->left);
+            }
+        }
+
         return nodes;
     }
 };
