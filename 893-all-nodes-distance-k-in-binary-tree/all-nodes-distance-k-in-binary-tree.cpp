a6b31bb42a852487f7078721b6d87dba7a318826
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-07-28 13:59:21 +0530

    Time: 8 ms (48.84%) | Memory: 13.4 MB (33.19%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -1,47 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
-private:
-    unordered_map<int, vector<int>> graph;
-    vector<int> answer;
+
+    unordered_map<int,vector<int>> mpp;
+    vector<int> nodes;
     unordered_set<int> visited;
+
+    void buildGraph(TreeNode *root, TreeNode *parent){
+        if(root != nullptr && parent != nullptr){
+            mpp[root->val].push_back(parent->val);
+            mpp[parent->val].push_back(root->val);
+        }
+        if(root->left){
+            buildGraph(root->left, root);
+        }
+
+        if(root->right){
+            buildGraph(root->right, root);
+        }
+    }
+
+    void dfs(int node, int k){
+        if(k == 0){
+            nodes.push_back(node);
+            return;
+        }
+
+        for(int element : mpp[node]){
+            if(visited.find(element) == visited.end()){
+                visited.insert(element);
+                dfs(element, k - 1);
+            }
+        }
+    }
 
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        buildGraph(root, NULL);
-
-        answer.clear();
-        visited.clear();
+        buildGraph(root, nullptr);
         visited.insert(target->val);
-
-        dfs(target->val, 0, k);
-
-        return answer;
-    }
-
-private:
-    // Recursively build the undirected graph from the given binary tree.
-    void buildGraph(TreeNode* cur, TreeNode* parent) {
-        if (cur != nullptr && parent != nullptr) {
-            graph[cur->val].push_back(parent->val);
-            graph[parent->val].push_back(cur->val);
-        }
-        if (cur->left != nullptr) {
-            buildGraph(cur->left, cur);
-        }
-        if (cur->right != nullptr) {
-            buildGraph(cur->right, cur);
-        }
-    }
-
-    void dfs(int cur, int distance, int k) {
-        if (distance == k) {
-            answer.push_back(cur);
-            return;
-        }
-        for (int neighbor : graph[cur]) {
-            if (visited.find(neighbor) == visited.end()) {
-                visited.insert(neighbor);
-                dfs(neighbor, distance + 1, k);
-            }
-        }
+        dfs(target->val, k);
+        return nodes;
     }
 };
