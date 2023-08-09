0ef056e66e7d71856e8cc966845854ebfc1cdf4a
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-08-09 17:40:20 +0530

    Time: 12 ms (73.83%) | Memory: 16.2 MB (35.63%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -1,34 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    vector<TreeNode*> Helper(int start, int end) {
-        vector<TreeNode*> trees;
 
-        if (start > end) {
+    vector<TreeNode*> Helper(int start, int end){
+        vector<TreeNode*> trees;
+        if(start > end || start <= 0){
             trees.push_back(nullptr);
             return trees;
         }
 
-        for (int i = start; i <= end; i++) {
-            vector<TreeNode*> leftSubtrees = Helper(start, i - 1);
-            vector<TreeNode*> rightSubtrees = Helper(i + 1, end);
+        for(int i = start; i <= end; i++){
+            vector<TreeNode*> leftSubtree = Helper(start, i - 1);
+            vector<TreeNode*> rightSubtree = Helper(i + 1, end);
 
-            for (TreeNode* left : leftSubtrees) {
-                for (TreeNode* right : rightSubtrees) {
-                    TreeNode* root = new TreeNode(i);
+            for(auto left : leftSubtree){
+                for(auto right : rightSubtree){
+                    TreeNode *root = new TreeNode(i);
                     root->left = left;
                     root->right = right;
                     trees.push_back(root);
                 }
             }
+            
         }
+
 
         return trees;
     }
 
 public:
     vector<TreeNode*> generateTrees(int n) {
-        if (n == 0) {
+        if(n == 0){
             return vector<TreeNode*>();
         }
+
         return Helper(1, n);
     }
 };
