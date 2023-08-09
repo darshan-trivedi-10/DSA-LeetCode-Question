16eaf360fed48938bc5aae8cb155074808dcee31
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-08-09 12:52:44 +0530

    Time: 58 ms (80.23%) | Memory: 44.1 MB (11.44%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -2,29 +2,17 @@ class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
         int size = cardPoints.size();
-        vector<int> prefixSum(size, 0);
-        for(int i = 0; i < size; i++){
-            prefixSum[i] = cardPoints[i];
-            if(i > 0){
-                prefixSum[i] += prefixSum[i-1];
-            }
+        vector<int> prefixSum(size + 1, 0);  // Include an extra element for easier calculations
+        
+        for (int i = 0; i < size; i++) {
+            prefixSum[i + 1] = prefixSum[i] + cardPoints[i];  // Build prefix sum array
         }
 
-        for(auto it : prefixSum){
-            cout<<it<<" ";
-        }cout<<endl;
-
         int maxPoints = 0;
-        for(int i = 0; i <= k; i++){
+        for (int i = 0; i <= k; i++) {
             int le = i, re = k - i, currPoints = 0;
-            if(le > 0){
-                currPoints += prefixSum[le - 1];
-            }
-            if(re > 0){
-                cout<<prefixSum[size - 1]<<" "<<prefixSum[size - re]<<" ";
-                currPoints += prefixSum[size - 1] - (size-re-1 >= 0 ? prefixSum[size - re-1] : 0);
-            }
-            cout<<le<<" "<<re<<" "<<currPoints<<endl;
+            currPoints = prefixSum[le] + (prefixSum[size] - prefixSum[size - re]);
+
             maxPoints = max(maxPoints, currPoints);
         }
 
