76c25bb5996058c9a4a2f82fad99385ab5c6d2e6
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-08-19 10:56:48 +0530

    Time: 8 ms (24.26%) | Memory: 13.5 MB (44.78%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -1,41 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> ans;
-        if(root == nullptr){
-            return ans;
+    std::vector<std::string> binaryTreePaths(TreeNode* root) {
+        std::vector<std::string> paths;
+        if (root == nullptr) {
+            return paths;
         }
 
         int val = root->val;
-        vector<string> left = binaryTreePaths(root->left);
-        vector<string> right = binaryTreePaths(root->right);
+        auto left_paths = binaryTreePaths(root->left);
+        auto right_paths = binaryTreePaths(root->right);
 
-        for(auto str : left){
-            string path = to_string(val) + "->" + str;
-            ans.push_back(path);
+        for (const auto& path : left_paths) {
+            paths.push_back(std::to_string(val) + "->" + path);
         }
 
-        for(auto str : right){
-            string path = to_string(val) + "->" + str;
-            ans.push_back(path);
+        for (const auto& path : right_paths) {
+            paths.push_back(std::to_string(val) + "->" + path);
         }
 
-        if(ans.size() == 0){
-            string path = to_string(root->val);
-            ans.push_back(path);
+        if (paths.empty()) {
+            paths.push_back(std::to_string(val));
         }
 
-        return ans;
+        return paths;
     }
 };
