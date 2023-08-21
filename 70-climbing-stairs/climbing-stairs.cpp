513966a1d2b2d34465bc9657d7304f30373c80a3
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-08-21 13:46:57 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (16.16%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -13,7 +13,16 @@ class Solution {
 
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return climber(n, dp);
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1, dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1];
+            if(i > 1){
+                dp[i] += dp[i-2];
+            }
+        }
+
+        return dp[n];
     }
 };
