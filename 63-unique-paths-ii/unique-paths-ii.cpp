d23368cc0387785e1f6c47e4cfe7cab6ae2ba996
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-08-22 17:54:15 +0530

    Time: 5 ms (36.70%) | Memory: 7.7 MB (80.45%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -33,24 +33,26 @@ public:
         // vector<vector<int>> dp(n, vector<int> (m, -1));
         // return findPath(0, 0, n, m, obstacleGrid, dp);
 
-        vector<vector<int>> dp(n, vector<int> (m, 0));
+        vector<int> prev(m, 0);
         for(int i  = 0; i < n; i++){
+            vector<int> curr(m, 0);
             for(int j = 0; j < m; j++){
                 if(obstacleGrid[i][j] == 1){
                     continue;
                 }
                 if(i == 0 && j == 0){
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                 }
                 if(i > 0){
-                    dp[i][j] += dp[i-1][j];
+                    curr[j] += prev[j];
                 }
                 if(j > 0){
-                    dp[i][j] += dp[i][j-1];
+                    curr[j] += curr[j-1];
                 }
             }
+            prev = curr;
         }
 
-        return dp[n - 1][m - 1];
+        return prev[m - 1];
     }
 };
