316d1d60d33e3e7c47a9f255179c3377ed35d6e8
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-08-22 16:35:58 +0530

    Time: 3 ms (34.64%) | Memory: 6.6 MB (5.59%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,31 @@
+class Solution {
+
+    bool isValid(int i, int j, int n, int m){
+        return (i < n and j < m);
+    }
+    
+    int findPath(int i, int j, int n, int m, vector<vector<int>> &dp){
+        if(i == n - 1 and j == m - 1){
+            return 1;
+        }
+
+        if(!isValid(i, j, n, m)){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int down = findPath(i + 1, j, n, m, dp);
+        int right = findPath(i, j + 1, n, m, dp);        
+
+        return dp[i][j] = down + right;
+    }
+
+public:
+    int uniquePaths(int n, int m) {
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return findPath(0, 0, n, m, dp);
+    }
+};
