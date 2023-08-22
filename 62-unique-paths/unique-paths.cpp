1eb3833c4daea056e4bf1f548607a5f42e196008
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-08-22 17:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (70.68%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -28,23 +28,27 @@ public:
         // vector<vector<int>> dp(n, vector<int> (m, -1));
         // return findPath(0, 0, n, m, dp);
 
-        vector<vector<int>> dp(n, vector<int> (m, 0));
+        // vector<vector<int>> dp(n, vector<int> (m, 0));
+
+        vector<int> prev(m, 0);
 
         for(int i = 0; i < n; i++){
+            vector<int> curr(m, 0);
             for(int j = 0; j < m; j++){
                 if(i == 0 && j == 0){
-                    dp[i][j] = 1;
-                    continue;
+                    curr[j] = 1;
                 }
                 if(i > 0){
-                    dp[i][j] += dp[i-1][j];
+                    curr[j] += prev[j];
                 }
                 if(j > 0){
-                    dp[i][j] += dp[i][j-1];
+                    curr[j] += curr[j-1];
                 }
             }
+
+            prev = curr;
         }
 
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
