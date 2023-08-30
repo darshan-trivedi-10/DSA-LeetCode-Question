35dfcacd0086dac176c771862f8ff788c1a36c20
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-08-30 14:09:33 +0530

    Time: 495 ms (40.87%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,3 +1,3 @@
 # Cinema (id, movie, description, rating)
 SELECT * FROM Cinema 
-WHERE id%2 = 1 AND description != 'boring' ORDER BY rating DESC;
+WHERE mod(id, 2) = 1 AND description != 'boring' ORDER BY rating DESC;
