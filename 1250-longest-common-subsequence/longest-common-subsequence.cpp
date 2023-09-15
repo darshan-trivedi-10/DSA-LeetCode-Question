70246062b0e24de176e467828bfe8bcc81f54609
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2023-09-15 10:48:40 +0530

    Time: 133 ms (5.18%) | Memory: 24.8 MB (5.44%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,50 +1,59 @@
 class Solution {
 
-    int Helper(string &text1, string &text2, int i, int j, vector<vector<int>> &dp){
+    // int Helper(string &text1, string &text2, int i, int j, vector<vector<int>> &dp){
 
-        if(i == text1.size() || j == text2.size()){
-            return 0;
-        }
+    //     if(i == text1.size() || j == text2.size()){
+    //         return 0;
+    //     }
 
-        if(dp[i][j] != -1){
-            return -1;
-        }
+    //     if(dp[i][j] != -1){
+    //         return -1;
+    //     }
 
-        int taken = 0, notTaken = 0;
-        if(text1[i] == text2[j]){
-            taken = 1 + Helper(text1, text2, i + 1, j + 1, dp);
-        }else{
-            notTaken = max(
-                Helper(text1, text2, i + 1, j, dp), 
-                Helper(text1, text2,  i, j + 1, dp)
-            );
-        }
+    //     int taken = 0, notTaken = 0;
+    //     if(text1[i] == text2[j]){
+    //         taken = 1 + Helper(text1, text2, i + 1, j + 1, dp);
+    //     }else{
+    //         notTaken = max(
+    //             Helper(text1, text2, i + 1, j, dp), 
+    //             Helper(text1, text2,  i, j + 1, dp)
+    //         );
+    //     }
 
-        return dp[i][j] = max(taken, notTaken);
+    //     return dp[i][j] = max(taken, notTaken);
+    // }
+
+    int Helper(string& text1, string& text2, int i, int j, vector<vector<int>>& dp) {
+    if (i == text1.size() || j == text2.size()) {
+        return 0;
     }
 
-    int lcsUtil(string& s1, string& s2, int ind1, int ind2,  vector<vector<int>>& dp){
+    if (dp[i][j] != -1) {
+        return dp[i][j];
+    }
 
-    if(ind1<0 || ind2<0)
-        return 0;
-        
-    if(dp[ind1][ind2]!=-1)
-        return dp[ind1][ind2];
-    
-    if(s1[ind1] == s2[ind2])
-        return dp[ind1][ind2] = 1 + lcsUtil(s1,s2,ind1-1,ind2-1,dp);
-    
-    else 
-        return dp[ind1][ind2] = 0 + max(lcsUtil(s1,s2,ind1,ind2-1,dp),lcsUtil
-       (s1,s2,ind1-1,ind2,dp));
-   
+    int taken = 0, notTaken = 0;
+    if (text1[i] == text2[j]) {
+        taken = 1 + Helper(text1, text2, i + 1, j + 1, dp);
+    } else {
+        taken = Helper(text1, text2, i + 1, j + 1, dp); // Continue exploring even if characters don't match
+    }
+
+    notTaken = max(
+        Helper(text1, text2, i + 1, j, dp),
+        Helper(text1, text2, i, j + 1, dp)
+    );
+
+    return dp[i][j] = max(taken, notTaken);
 }
 
+
+   
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int i = text1.size(), j = text2.size();
         vector<vector<int>> dp(i, vector<int> (j, -1));
-        return lcsUtil(text1, text2, i - 1, j - 1, dp);
-        // return Helper(text1, text2, 0, 0, dp);
+        // return lcsUtil(text1, text2, i - 1, j - 1, dp);
+        return Helper(text1, text2, 0, 0, dp);
     }
 };
